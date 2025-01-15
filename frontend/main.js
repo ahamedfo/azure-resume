dc3f9606a8d2d94030e3ef34b384998abe696628
Ahamed Fofana <ahamedfofana12@gmail.com>
2025-01-15 08:30:00 -0500

    deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = "https://getresumecounteramed.azurewebsites.net/api/GetResumeCounter?code=02PYYd_LlZO5k2xzQUxed2y-z7vDe3Nd-Y3ckWqwzog7AzFuZBbaIA%3D%3D";
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("website called function API")
